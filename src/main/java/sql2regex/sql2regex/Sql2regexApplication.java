39179600498fa683f846e3efd6e37d1af261e2a6
binkertpat <58105072+binkertpat@users.noreply.github.com>
2022-02-07 17:33:35 +0100

    init spring boot

---
+++ src/main/java/sql2regex/sql2regex/Sql2regexApplication.java
@@ -0,0 +1,13 @@
+package sql2regex.sql2regex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sql2regexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sql2regexApplication.class, args);
+	}
+
+}
