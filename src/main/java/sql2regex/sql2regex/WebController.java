386a55b706427e016402996a3560510981a6c17d
binkertpat <58105072+binkertpat@users.noreply.github.com>
2022-03-13 15:56:16 +0100

    exclude header, footer, ..; add examples page; add examples check

--- src/main/java/sql2regex/sql2regex/WebController.java
+++ src/main/java/sql2regex/sql2regex/WebController.java
@@ -1,13 +1,30 @@
 package sql2regex.sql2regex;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
     @GetMapping("/")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("title", "sql2regex");
+        model.addAttribute("activeConverter", true);
         return "home";
+    }
+
+    @GetMapping("/examples")
+    public String examples(Model model) {
+        model.addAttribute("title", "sql2regex - examples");
+        model.addAttribute("activeExamples", true);
+        return "examples";
+    }
+
+    @GetMapping("/about")
+    public String aboutus(Model model) {
+        model.addAttribute("title", "sql2regex - about us");
+        model.addAttribute("activeAbout", true);
+        return "about";
     }
 }
 
--- src/main/resources/static/js/js_utils.js
+++ src/main/resources/static/js/js_utils.js
@@ -57,3 +57,28 @@ function generateJsonFormatFile(id_sql, id_regex, id_jsonFeedback){
     var jsonFeedbackAlert = document.getElementById(id_jsonFeedback);
     jsonFeedbackAlert.style.display = "block";  
 }
+
+function setActiveClassToNavBar(parentnode){
+    var parentNode = document.getElementById(parentnode);
+    var children = parentNode.children;
+
+    for (var i = 0; i < children.length; i++) {
+        children[i].classList.remove("active");
+
+        var navbarLink = children[i].getAttribute("href");
+        var currentLink = window.location.href;
+
+        if(currentLink.includes(navbarLink) && navbarLink !== "/"){
+            children[i].classList.add("active");
+        } else if(navbarLink === "/") {
+            children[0].classList.add("active");
+        }
+    }
+}
+
+function checkRegExExample(sql, regex){
+    let text = document.getElementById(sql).value;
+    let pattern = new RegExp(document.getElementById(regex).innerHTML, 'gmi');
+    let result = pattern.test(text);
+    console.log(result);
+}
