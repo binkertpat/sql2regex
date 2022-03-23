95efa8fa1930efd6f075183e0d2469c6e833846d
binkertpat <58105072+binkertpat@users.noreply.github.com>
2022-03-24 00:38:33 +0100

    add functionality for showing converting history (stored in local storage)

--- src/main/java/sql2regex/sql2regex/Sql2regexApplication.java
+++ src/main/java/sql2regex/sql2regex/Sql2regexApplication.java
@@ -1,22 +1,17 @@
 package sql2regex.sql2regex;
 
 import java.util.Arrays;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
-import sql2regex.sql2regex.sql.SqlRegex;
 
 @SpringBootApplication
 public class Sql2regexApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sql2regexApplication.class, args);
-		SqlRegex sqlregex = new SqlRegex();
-		sqlregex.setSql("SELECT * FROM table");
-		System.out.println(sqlregex.getSql());
 	}
 
 	@Bean
--- src/main/resources/static/js/js_utils.js
+++ src/main/resources/static/js/js_utils.js
@@ -29,23 +29,24 @@ function copy2clipbord(id, idFeedback) {
         copyFeedbackAlert.style.display = "block";  
 }
 
+function formattedCurrentTimestamp(){
+    var currentdate = new Date();
+    var datetime =  ((currentdate.getDate().toString().length == 1) ? "0".concat(currentdate.getDate()) : (currentdate.getDate())) + "/"
+                    + (((currentdate.getMonth()+1).toString().length == 1) ? "0".concat(currentdate.getMonth()+1) : (currentdate.getMonth()+1)) + "/"
+                    + ((currentdate.getFullYear().toString().length == 1) ? "0".concat(currentdate.getFullYear()) : (currentdate.getFullYear())) + " @ "
+                    + ((currentdate.getHours().toString().length == 1) ? "0".concat(currentdate.getHours()) : (currentdate.getHours())) + ":"
+                    + ((currentdate.getMinutes().toString().length == 1) ? "0".concat(currentdate.getMinutes()) : (currentdate.getMinutes())) + ":"
+                    + ((currentdate.getSeconds().toString().length == 1) ? "0".concat(currentdate.getSeconds()) : (currentdate.getSeconds()));
+    return datetime;
+}
+
 function generateJsonFormatFile(id_sql, id_regex, id_jsonFeedback){
     var element = document.createElement('a');
-
     var sql = document.getElementById(id_sql).value;
     var regex = document.getElementById(id_regex).value;
 
-    var currentdate = new Date();  
-    var datetime =  ((currentdate.getDate().toString().length == 1) ? "0".concat(currentdate.getDate()) : (currentdate.getDate())) + "/" 
-                    + (((currentdate.getMonth()+1).toString().length == 1) ? "0".concat(currentdate.getMonth()+1) : (currentdate.getMonth()+1)) + "/" 
-                    + ((currentdate.getFullYear().toString().length == 1) ? "0".concat(currentdate.getFullYear()) : (currentdate.getFullYear())) + " @ "  
-                    + ((currentdate.getHours().toString().length == 1) ? "0".concat(currentdate.getHours()) : (currentdate.getHours())) + ":"  
-                    + ((currentdate.getMinutes().toString().length == 1) ? "0".concat(currentdate.getMinutes()) : (currentdate.getMinutes())) + ":"  
-                    + ((currentdate.getSeconds().toString().length == 1) ? "0".concat(currentdate.getSeconds()) : (currentdate.getSeconds()))  
-
-    var jsonformat = '{"sql":"' + sql + '", "regex":"' + regex +'", "website":"sql2regex.herokuapp.com", "timestamp":"' + datetime + '"}'
-    
-    var filename = "sql2regex_" + datetime.replaceAll(" @ ", "").replaceAll("/", "").replaceAll(":", "") + ".json";
+    var jsonformat = '{"sql":"' + sql + '", "regex":"' + regex +'", "website":"sql2regex.herokuapp.com", "timestamp":"' + formattedCurrentTimestamp() + '"}'
+    var filename = "sql2regex_" + formattedCurrentTimestamp().replaceAll(" @ ", "").replaceAll("/", "").replaceAll(":", "") + ".json";
 
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(jsonformat));
     element.setAttribute('download', filename);
@@ -87,3 +88,91 @@ function onChangePresetSelect(textinputfieldid, selectfieldid){
     let selectfield = document.getElementById(selectfieldid).options;
     textinputfield.value = selectfield[selectfield.selectedIndex].value;
 }
+
+class SqlRegExHistory {
+  constructor(localStorageId) {
+    let ls = localStorage.getItem(localStorageId);
+    this.localStorageId = localStorageId;
+    if(ls != null){
+        this.sql = JSON.parse(ls)[0];
+        this.regex = JSON.parse(ls)[1];
+    } else {
+        this.sql = new Array();
+        this.regex = new Array();
+        this.writeToLocalStorage(this);
+    }
+  }
+
+  writeToLocalStorage(data){
+    let toWrittenArrayOfArray = [this.sql, this.regex];
+    localStorage.setItem(this.localStorageId, JSON.stringify(toWrittenArrayOfArray));
+  }
+
+  readSqlFromLocalStorage(){
+    if(this.sql.length != 0) {
+        let ls = localStorage.getItem(this.localStorageId);
+        return JSON.parse(ls)[0];
+    } else return;
+  }
+
+  readRegExFromLocalStorage(){
+    if(this.regex.length != 0) {
+      let ls = localStorage.getItem(this.localStorageId);
+      return JSON.parse(ls)[1];
+    } else return;
+  }
+
+  readSqlRegExFromLocalStorage(){
+      return [this.readSqlFromLocalStorage(), this.readRegExFromLocalStorage()]
+  }
+
+  addToLocalStorage(sqlNew, regexNew){
+      this.sql.push(sqlNew)
+      this.regex.push(regexNew)
+      this.writeToLocalStorage()
+  }
+
+  checkUpdatedConverting(){
+    let sqlinput = document.getElementById("sqlinput").value;
+    let regexinput = document.getElementById("regexoutput").value;
+    if(!this.sql.includes(sqlinput) && sqlinput.length != 0) this.addToLocalStorage(sqlinput, regexinput);
+  }
+
+  showConvertingHistory(){
+       let arrayOfSqlAndArrayOfRegex = this.readSqlRegExFromLocalStorage();
+
+       for(var i = 0; i<arrayOfSqlAndArrayOfRegex.length; i++){
+            console.log(arrayOfSqlAndArrayOfRegex[0][i]);
+            console.log(arrayOfSqlAndArrayOfRegex[1][i]);
+       }
+  }
+
+  clearLocalStorage(){
+      localStorage.removeItem(this.localStorageId);
+  }
+
+  downloadJsonOfHistory(){
+        let ConertingHistory = {}
+        for(var i = 0; i<this.readSqlRegExFromLocalStorage()[0].length; i++){
+            let SingleConvert = {};
+            SingleConvert["sql"] = this.readSqlRegExFromLocalStorage()[0][i];
+            SingleConvert["regex"] = this.readSqlRegExFromLocalStorage()[1][i];
+            ConertingHistory[i] = SingleConvert;
+        }
+        ConertingHistory["website"] = "sql2regex.herokuapp.com";
+        ConertingHistory["timestamp"] =  formattedCurrentTimestamp();
+
+        let filename = "sql2regex_convertinghistory_" + formattedCurrentTimestamp().replaceAll(" @ ", "").replaceAll("/", "").replaceAll(":", "") + ".json";
+        let element = document.createElement('a');
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(ConertingHistory)));
+        element.setAttribute('download', filename);
+        element.style.display = 'none';
+        document.body.appendChild(element);
+        element.click();
+        document.body.removeChild(element);
+  }
+}
+
+let SqlRegExHis = new SqlRegExHistory("SqlRegExHistory");
+SqlRegExHis.checkUpdatedConverting();
+console.log(SqlRegExHis.readSqlRegExFromLocalStorage());
