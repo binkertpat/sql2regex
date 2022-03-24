f1d6e5454c461afe7cc17deb628ccb4f5b61905c
binkertpat <58105072+binkertpat@users.noreply.github.com>
2022-03-24 01:50:04 +0100

    add converting history to frontend

--- src/main/resources/static/js/js_utils.js
+++ src/main/resources/static/js/js_utils.js
@@ -108,17 +108,17 @@ class SqlRegExHistory {
   }
 
   readSqlFromLocalStorage(){
-    if(this.sql.length != 0) {
+    if(localStorage.getItem(this.localStorageId) != null) {
         let ls = localStorage.getItem(this.localStorageId);
         return JSON.parse(ls)[0];
-    } else return;
+    } else return -1;
   }
 
   readRegExFromLocalStorage(){
-    if(this.regex.length != 0) {
+    if(localStorage.getItem(this.localStorageId) != null) {
       let ls = localStorage.getItem(this.localStorageId);
       return JSON.parse(ls)[1];
-    } else return;
+    } else return -1;
   }
 
   readSqlRegExFromLocalStorage(){
@@ -132,18 +132,80 @@ class SqlRegExHistory {
   }
 
   checkUpdatedConverting(){
+    document.getElementById("convertbodycontainer").style.display = "none";
     let sqlinput = document.getElementById("sqlinput").value;
     let regexinput = document.getElementById("regexoutput").value;
     if(!this.sql.includes(sqlinput) && sqlinput.length != 0) this.addToLocalStorage(sqlinput, regexinput);
+    if(sqlinput.length != 0) this.showConvertingHistory();
   }
 
   showConvertingHistory(){
+       document.getElementById("convertbodycontainer").style.display = "block";
        let arrayOfSqlAndArrayOfRegex = this.readSqlRegExFromLocalStorage();
+       let body = document.getElementById("convertbody");
 
-       for(var i = 0; i<arrayOfSqlAndArrayOfRegex.length; i++){
-            console.log(arrayOfSqlAndArrayOfRegex[0][i]);
-            console.log(arrayOfSqlAndArrayOfRegex[1][i]);
-       }
+        let table = document.createElement('table');
+        table.classList.add("table");
+        table.classList.add("table-hover");
+        let thead = document.createElement('thead');
+        let tbody = document.createElement('tbody');
+
+        let row1 = document.createElement('tr');
+        let heading1 = document.createElement('th');
+        heading1.setAttribute("width", "6%");
+        heading1.innerHTML = "Id";
+        let heading2 = document.createElement('th');
+        heading2.setAttribute("width", "47%");
+        heading2.innerHTML = "SQL-Eingabe";
+        let heading3 = document.createElement('th');
+        heading3.setAttribute("width", "47%");
+        heading3.innerHTML = "RegEx-Ausgabe";
+        row1.appendChild(heading1);
+        row1.appendChild(heading2);
+        row1.appendChild(heading3);
+        thead.appendChild(row1);
+
+        if(arrayOfSqlAndArrayOfRegex[0] != -1){
+            for(var i = 0; i<this.sql.length; i++){
+                console.log(arrayOfSqlAndArrayOfRegex[0][i]);
+                console.log(arrayOfSqlAndArrayOfRegex[1][i]);
+
+                let row_data = document.createElement('tr');
+                let row_data_1 = document.createElement('td');
+                row_data_1.innerHTML = i;
+                let row_data_2 = document.createElement('td');
+                row_data_2.innerHTML = arrayOfSqlAndArrayOfRegex[0][i];
+                let row_data_3 = document.createElement('td');
+                row_data_3.innerHTML = arrayOfSqlAndArrayOfRegex[1][i];
+
+                row_data.appendChild(row_data_1);
+                row_data.appendChild(row_data_2);
+                row_data.appendChild(row_data_3);
+                tbody.appendChild(row_data);
+            }
+        }
+
+        table.appendChild(thead);
+        table.appendChild(tbody);
+
+        let tableHeading = document.createElement('h2');
+        tableHeading.innerHTML = "Converting-History";
+        tableHeading.classList.add("pb-2");
+        tableHeading.classList.add("border-bottom");
+
+
+        let exportButtonContainer = document.createElement('div');
+        exportButtonContainer.classList.add("text-end");
+
+        let exportButton = document.createElement('button');
+        exportButton.innerHTML = "export as .json file";
+        exportButton.classList.add("btn","btn-m","btn-success","copy-button", "mt-2");
+        exportButton.setAttribute("onclick", "SqlRegExHis.downloadJsonOfHistory()")
+        exportButtonContainer.appendChild(exportButton);
+
+        body.appendChild(tableHeading);
+        body.appendChild(table);
+        body.appendChild(exportButtonContainer);
   }
 
   clearLocalStorage(){
@@ -151,19 +213,22 @@ class SqlRegExHistory {
   }
 
   downloadJsonOfHistory(){
-        let ConertingHistory = {}
+        let converts = {}
+        let ConvertingHistory = {}
+
         for(var i = 0; i<this.readSqlRegExFromLocalStorage()[0].length; i++){
             let SingleConvert = {};
             SingleConvert["sql"] = this.readSqlRegExFromLocalStorage()[0][i];
             SingleConvert["regex"] = this.readSqlRegExFromLocalStorage()[1][i];
-            ConertingHistory[i] = SingleConvert;
+            converts[i] = SingleConvert;
         }
-        ConertingHistory["website"] = "sql2regex.herokuapp.com";
-        ConertingHistory["timestamp"] =  formattedCurrentTimestamp();
+        ConvertingHistory["results"] = converts;
+        ConvertingHistory["website"] = "sql2regex.herokuapp.com";
+        ConvertingHistory["timestamp"] =  formattedCurrentTimestamp();
 
         let filename = "sql2regex_convertinghistory_" + formattedCurrentTimestamp().replaceAll(" @ ", "").replaceAll("/", "").replaceAll(":", "") + ".json";
         let element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(ConertingHistory)));
+        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(JSON.stringify(ConvertingHistory)));
         element.setAttribute('download', filename);
         element.style.display = 'none';
         document.body.appendChild(element);
